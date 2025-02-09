9b1dc83a07ba4003600336cda25be26e299b4ec8
AbhijeetKhairnar02 <165133977+AbhijeetKhairnar02@users.noreply.github.com>
2025-02-09 21:32:25 +0530

    Update settings.py

--- Library Management  Project/backend/backend/settings.py
+++ Library Management  Project/backend/backend/settings.py
@@ -139,7 +139,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'pravinkorde8999@gmail.com'
+EMAIL_HOST_USER = 'abhijhairnar03@gmail.com'
 EMAIL_HOST_PASSWORD = "your-email-password"
 
 
